a8cd9197124c9958831e6c71d6e9a37869c52bb5
dodopontocom <rodolfotiago@gmail.com>
2018-12-07 00:44:34 -0200

    f

--- webserver-container-cluster.tf
+++ webserver-container-cluster.tf
@@ -1,9 +1,20 @@
-resource "google_container_cluster" "primary" {
-  name         = "gke-tf-${random_id.instance_id.hex}"
-  //machine_type = "f1-micro"
-  zone         = "us-central1-a"
-  initial_node_count = 3
+resource "google_container_node_pool" "np" {
+  name       = "node-pool-tf-${random_id.instance_id.hex}"
+  zone       = "us-central1-a"
+  cluster    = "${google_container_cluster.primary.name}"
+  node_count = 3
 
+  node_config {
+    machine_type = "f1-micro"
+    disk_size_gb = 10
+    preemptible  = true
+  }
+}
+resource "google_container_cluster" "primary" {
+  name                = "gke-tf-${random_id.instance_id.hex}"
+  zone                = "us-central1-a"
+  initial_node_count  = 3
+  
   addons_config {
     http_load_balancing {
       disabled = true
