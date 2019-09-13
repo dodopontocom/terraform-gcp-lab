6bb62b8961576a8d8379184847bf3b07ec4e2d78
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-13 13:56:41 +0000

    test

--- .circleci/definitions.sh
+++ .circleci/definitions.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+ROOTDIR="$(cd $(dirname ${BASH_SOURCE[0]})/../ >/dev/null 2>&1 && pwd )"
+
+export GCLOUD_DEVOPS_SA_PATH=${ROOTDIR}/gcp/credentials/credential.json
+
 export TF_VAR_project_id=${GCLOUD_PRODJECT_ID}
 export TF_VAR_region=${GCLOUD_PRODJECT_REGION}
-export TF_VAR_service_account_credential_path=${GOOGLE_APPLICATION_CREDENTIALS}
+export TF_VAR_service_account_credential_path=${GCLOUD_DEVOPS_SA_PATH}
