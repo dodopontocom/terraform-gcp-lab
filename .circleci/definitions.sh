d3c07a43c883ec7697b85cfab8f7dc5fdc04d3bf
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-13 17:28:09 +0000

    45545

--- .circleci/definitions.sh
+++ .circleci/definitions.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-CIRCLE_CI_BASEDIR="/root/project/"
-
-export GCLOUD_DEVOPS_SA="$(find ${BASEDIR} -iname ".gcp-credentials")/credential.json"
+export GCLOUD_DEVOPS_SA="$(find ${CIRCLE_WORKING_DIRECTORY} -iname ".gcp-credentials")/credential.json"
 
 export TF_VAR_project_id="${GCLOUD_PRODJECT_ID}"
 export TF_VAR_region="${GCLOUD_PRODJECT_REGION}"
