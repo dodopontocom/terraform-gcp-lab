7fb8cb8f7dec78d55dbee7ad6f82e78bba61fc36
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-13 14:21:01 +0000

    tr

--- .circleci/definitions.sh
+++ .circleci/definitions.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export GCLOUD_DEVOPS_SA_PATH=./gcp/credentials/credential.json
+BASEDIR="$(dirname $0)"
+echo "basedir: ${BASEDIR}"
+
+export GCLOUD_DEVOPS_SA_PATH=${BASEDIR}/gcp/credentials/credential.json
 
 export TF_VAR_project_id=${GCLOUD_PRODJECT_ID}
 export TF_VAR_region=${GCLOUD_PRODJECT_REGION}
-export TF_VAR_service_account_credential_path=./gcp/credentials/credential.json
+export TF_VAR_service_account_credential_path=${GCLOUD_DEVOPS_SA_PATH}
