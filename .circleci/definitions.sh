8ee8f2f74a0633cb36cbe03f9cd0c7236505856c
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-13 14:13:59 +0000

    atd

--- .circleci/definitions.sh
+++ .circleci/definitions.sh
@@ -4,4 +4,4 @@ export GCLOUD_DEVOPS_SA_PATH=./gcp/credentials/credential.json
 
 export TF_VAR_project_id=${GCLOUD_PRODJECT_ID}
 export TF_VAR_region=${GCLOUD_PRODJECT_REGION}
-export TF_VAR_service_account_credential_path=${GCLOUD_DEVOPS_SA_PATH}
+export TF_VAR_service_account_credential_path=./gcp/credentials/credential.json
