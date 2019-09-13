4fcde82cbab05b457664c26524a58492864bda25
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-13 17:22:42 +0000

    dasdad

--- .circleci/definitions.sh
+++ .circleci/definitions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export GCLOUD_DEVOPS_SA="$(find ../ -iname ".gcp-credentials")/credential.json"
+BASEDIR="$(cd $(dirname ${BASH_SOURCE[0]})/../ >/dev/null 2>&1 && pwd )"
+
+export GCLOUD_DEVOPS_SA="$(find ${BASEDIR} -iname ".gcp-credentials")/credential.json"
 
 export TF_VAR_project_id="${GCLOUD_PRODJECT_ID}"
 export TF_VAR_region="${GCLOUD_PRODJECT_REGION}"
