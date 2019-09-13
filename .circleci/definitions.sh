38baf30d093181e5efe226c41962599464ef4d0b
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-13 16:58:32 +0000

    bbb

--- .circleci/definitions.sh
+++ .circleci/definitions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export GCLOUD_DEVOPS_SA_PATH=./gcp-credentials/credential.json
+export GCLOUD_DEVOPS_SA="./terraform/gcp-credentials/credential.json"
 
-export TF_VAR_project_id=${GCLOUD_PRODJECT_ID}
-export TF_VAR_region=${GCLOUD_PRODJECT_REGION}
-export TF_VAR_service_account_credential_path=${GCLOUD_DEVOPS_SA_PATH}
+export TF_VAR_project_id="${GCLOUD_PRODJECT_ID}"
+export TF_VAR_region="${GCLOUD_PRODJECT_REGION}"
+export TF_VAR_service_account_credential_path="${GCLOUD_DEVOPS_SA}"
