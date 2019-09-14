2a283d5a21812b67f18500ab8124d7fcdc793fa2
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 00:18:57 -0300

    dçald

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,6 +1,13 @@
-resource "google_project_services" "project" {
- project = "${var.project_id}"
- services = [
-   "compute.googleapis.com"
- ]
+resource "null_resource" "resource-to-wait-on" {
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+  depends_on = ["google_project_service.gke-api"]
+}
+
+resource "google_project_service" "gke-api" {
+  project = "gcp-laboratories"
+  service = "container.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = false
 }
