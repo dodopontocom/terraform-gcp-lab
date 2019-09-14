e92ffec42a72a3a504865e07193119da80c67b8b
rodolfotiago <rodolfotiago@gmail.com>
2019-09-13 23:06:52 -0300

    nnn

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file(${var.service_account_credential_path})}"
+  credentials = "${file("${var.service_account_credential_path}")}"
   project     = "${var.project_id}"
   region      = "${var.region}"
 }
