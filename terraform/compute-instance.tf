a924d0ed64d0fb0f435debed9edd87f6d37879ee
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 20:06:56 -0300

    lalal

--- terraform/compute-instance.tf
+++ terraform/compute-instance.tf
@@ -6,6 +6,10 @@ resource "google_compute_instance" "default" {
   name         = "vm-tf-${random_id.instance_id.hex}"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
+
+  labels       = {
+    "env" = "dev"
+  }
   
   boot_disk {
     initialize_params {
