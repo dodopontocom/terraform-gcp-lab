d9ff21cae8d6a06032d4a270e2f1f03655155eae
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 11:39:51 -0300

    go go go

--- terraform/enable-apis.tf
+++ terraform/enable-apis.tf
@@ -18,3 +18,17 @@ resource "google_project_service" "gke-api" {
   disable_dependent_services = true
   disable_on_destroy         = false
 }
+
+resource "google_project_service" "scheduler-api" {
+  project = "${var.project_id}"
+  service = "cloudscheduler.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
+
+resource "google_project_service" "pubsub-api" {
+  project = "${var.project_id}"
+  service = "pubsub.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = false
+}
