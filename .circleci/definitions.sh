f4a266d7de40e9920f37bdb22aaf91b50a7eef56
rodolfotiago <rodolfotiago@gmail.com>
2019-09-13 22:36:49 -0300

    ok

--- .circleci/definitions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export GCLOUD_DEVOPS_SA="$(find ${CIRCLE_WORKING_DIRECTORY}/ -iname ".gcp-credentials")/credential.json"
-
-export TF_VAR_project_id="${GCLOUD_PRODJECT_ID}"
-export TF_VAR_region="${GCLOUD_PRODJECT_REGION}"
-export TF_VAR_service_account_credential_path="${GCLOUD_DEVOPS_SA}"
