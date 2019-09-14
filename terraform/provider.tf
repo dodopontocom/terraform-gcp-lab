f1aa007920d9dbe0865ab993b0dd317cd09133f2
rodolfotiago <rodolfotiago@gmail.com>
2019-09-13 23:10:28 -0300

    d

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("/root/terraform-gcp-lab/.credentials/credential.jsona")}"
+  credentials = "${file("/root/terraform-gcp-lab/.credentials/credential.json")}"
   project     = "${var.project_id}"
   region      = "${var.region}"
 }
