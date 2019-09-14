c18a4c93818ece1cb7de27d1a355a9f0bf9471de
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 20:42:57 -0300

    gafve

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -1,11 +1,12 @@
 resource "google_cloud_scheduler_job" "stop_job" {
   name     = "${var.schedule_stop}"
   description = "stop test job"
-  schedule = "35 20 * * *"
+  schedule = "44 20 * * *"
   time_zone = "America/Sao_Paulo"
 
   pubsub_target {
-    topic_name = "${var.stop_topic}"
+    //topic_name = "${var.stop_topic}"
+    topic_name = "projects/gcp-laboratories/topics/stop-instance-event"
     //data = "${base64encode("test")}"
     data = "${base64encode("{\"zone\":\"us-central1-a\", \"label\":\"env=dev\"}")}"
   }
