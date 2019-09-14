e6fc16164ca2cb8799c7b59139d065c3b4ee6abb
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 00:23:22 -0300

    apeok

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,8 +1,15 @@
 resource "null_resource" "resource-to-wait-on" {
   provisioner "local-exec" {
-    command = "sleep 60"
+    command = "sleep 90"
   }
   depends_on = ["google_project_service.gke-api"]
+}
+
+resource "google_project_service" "crm-api" {
+  project = "${var.project_id}"
+  service = "cloudresourcemanager.googleapis.com"
+  disable_dependent_services = true
+  disable_on_destroy         = false
 }
 
 resource "google_project_service" "gke-api" {
