7fca46d9b447f8214a4be0c0a93574228141c2e2
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 20:32:51 -0300

    kapeiq

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -1,11 +1,12 @@
 resource "google_cloud_scheduler_job" "stop_job" {
   name     = "${var.schedule_stop}"
   description = "stop test job"
-  schedule = "30 20 * * *"
+  schedule = "35 20 * * *"
   time_zone = "America/Sao_Paulo"
 
   pubsub_target {
     topic_name = "${var.stop_topic}"
-    data = "{\"zone\":\"us-central1-a\", \"label\":\"env=dev\"}"
+    //data = "${base64encode("test")}"
+    data = "${base64encode("{\"zone\":\"us-central1-a\", \"label\":\"env=dev\"}")}"
   }
 }
