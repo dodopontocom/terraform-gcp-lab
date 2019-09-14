93ef94e268b46bfe3e6c494d6a3fd622ed52c2f9
rodolfotiago <rodolfotiago@gmail.com>
2019-09-14 20:28:56 -0300

    jpaj

--- terraform/scheduler.tf
+++ terraform/scheduler.tf
@@ -6,6 +6,6 @@ resource "google_cloud_scheduler_job" "stop_job" {
 
   pubsub_target {
     topic_name = "${var.stop_topic}"
-    data = "${base64encode('{"zone":"us-central1-a", "label":"env=dev"}')}"
+    data = "{\"zone\":\"us-central1-a\", \"label\":\"env=dev\"}"
   }
 }
