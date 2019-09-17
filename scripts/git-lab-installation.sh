4c18829d21492c99bbbe31299917091e8d53ace9
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-17 11:09:44 -0300

    Create git-lab-installation.sh
    [skip ci]

---
+++ scripts/git-lab-installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ref.: https://about.gitlab.com/install/#ubuntu
+
+sudo apt-get install -y curl openssh-server ca-certificates
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
+#GET VIRTUAL MACHINE IP
+VM_EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip
+)
+
+sudo EXTERNAL_URL="https://${VM_EXTERNAL_IP}" apt-get install gitlab-ce
