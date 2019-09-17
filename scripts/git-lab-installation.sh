0cfb2a80ae4ce9dab0869ddb7c7d484ab42ed7b6
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-17 13:36:29 -0300

    Update git-lab-installation.sh
    [skip ci]

--- scripts/git-lab-installation.sh
+++ scripts/git-lab-installation.sh
@@ -10,7 +10,7 @@ curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/scrip
 #GET VIRTUAL MACHINE IP
 VM_EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip
 )
-
+echo "============================= GITLAB INSTALLATION =========================================="
 sudo EXTERNAL_URL="https://${VM_EXTERNAL_IP}" apt-get install -y gitlab-ce
 if [[ "$?" -ne 0 ]]; then
   echo "============================= RE TRYING GITLAB INSTALLATION =========================================="
