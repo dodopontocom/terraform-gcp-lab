1fee42bf64173c59d553e3aa67439f5ebb4df700
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-17 12:12:43 -0300

    Update git-lab-installation.sh

--- scripts/git-lab-installation.sh
+++ scripts/git-lab-installation.sh
@@ -2,6 +2,7 @@
 # ref.: https://about.gitlab.com/install/#ubuntu
 
 sudo apt-get install -y curl openssh-server ca-certificates
+sudo apt-get install -y postfix
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 
 #GET VIRTUAL MACHINE IP
@@ -9,3 +10,6 @@ VM_EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetada
 )
 
 sudo EXTERNAL_URL="https://${VM_EXTERNAL_IP}" apt-get install gitlab-ce
+if [[ "$?" -ne 0 ]]; then
+  echo "============================= FAILED TO START GITLAB =========================================="
+fi
