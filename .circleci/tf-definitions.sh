cccadefc4f549078b2cb76e407db982b57b02d0b
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-18 14:34:19 -0300

    Update tf-definitions.sh
    [skip ci]

--- .circleci/tf-definitions.sh
+++ .circleci/tf-definitions.sh
@@ -2,6 +2,7 @@
 export TF_VAR_gcp_bucket="gcp-laboratories-tf-state"
 export TF_VAR_project_id="gcp-laboratories"
 export TF_VAR_region="us-central1"
+export TF_VAR_key="/root/terraform-gcp-lab/credentials/credential.json"
 export TF_VAR_start_topic="start-instance-event"
 export TF_VAR_stop_topic="stop-instance-event"
 export TF_VAR_start_function_name="startInstancePubSub"
@@ -14,5 +15,3 @@ export TF_VAR_machine_type="f1-micro"
 export TF_VAR_zone="us-central1-a"
 export TF_VAR_compute_instance_environment="dev"
 export TF_VAR_ubuntu_image="ubuntu-os-cloud/ubuntu-1804-lts"
-
-export TF_VAR_key=/root/terraform-gcp-lab/credentials/credential.json
