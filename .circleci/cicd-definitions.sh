49fb1adc96ce1f84e6d0d9d548cff3cb6adf0c38
Rodolfo Neto <rodolfotiago@gmail.com>
2019-09-27 11:18:59 -0300

    [skip ci]

--- .circleci/cicd-definitions.sh
+++ .circleci/cicd-definitions.sh
@@ -1,14 +1,18 @@
 # Terraform variables
-export TF_VAR_gcp_bucket="gcp-laboratories-tf-state"
-export TF_VAR_project_id="gcp-laboratories"
+
+# Gcloud project id
+export GCLOUD_PROJECT_ID="gcp-my-labs"
+
+export TF_VAR_gcp_bucket="${GCLOUD_PROJECT_ID}-tf-state"
+export TF_VAR_project_id="${GCLOUD_PROJECT_ID}"
 export TF_VAR_region="us-central1"
-export TF_VAR_key="/root/terraform-gcp-lab/credentials/credential.json"
+export TF_VAR_key="/root/${GCLOUD_PROJECT_ID}/credentials/credential.json"
 export TF_VAR_start_topic="start-instance-event"
 export TF_VAR_stop_topic="stop-instance-event"
 export TF_VAR_start_function_name="startInstancePubSub"
 export TF_VAR_stop_function_name="stopInstancePubSub"
 export TF_VAR_function_zip_name="index.zip"
-export TF_VAR_function_zip_source_file="/root/terraform-gcp-lab/gcp-functions/index.zip"
+export TF_VAR_function_zip_source_file="/root/${GCLOUD_PROJECT_ID}/gcp-functions/index.zip"
 export TF_VAR_schedule_stop="stop-job"
 export TF_VAR_schedule_start="start-job"
 export TF_VAR_machine_type="n1-standard-4"
