154c72dcdfd8be90a083e1ad02e0863b3f838c54
Rodolfo Neto <rodolfotiago@gmail.com>
2019-12-12 11:20:04 -0300

    [custom-vm]|odroid-contas-bot.sh

--- terraform/compute-instance.tf
+++ terraform/compute-instance.tf
@@ -29,8 +29,8 @@ resource "google_compute_instance" "gcp_lab_instance" {
   allow_stopping_for_update = true
   
   metadata = {
-    TELEGRAM_TOKEN = "${TELEGRAM_TOKEN}"
-    NOTIFICATION_IDS= 11504381
+    TELEGRAM_TOKEN = "${var.TELEGRAM_TOKEN}"
+    NOTIFICATION_IDS = "11504381"
   }
 
   metadata_startup_script = "${file("${var.startup_script}")}"
