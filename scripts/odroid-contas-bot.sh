5acbb5194bf242f427e515f3fff70990fa50438a
Rodolfo Neto <rodolfotiago@gmail.com>
2019-12-12 08:35:47 -0300

    [custom-vm]|odroid-contas-bot.sh

--- scripts/odroid-contas-bot.sh
+++ scripts/odroid-contas-bot.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-git_clone_url="https://github.com/dodopontocom/odroid-contas.git"
+project_name="odroid-contas"
+git_clone_url="https://github.com/dodopontocom/${project_name}.git"
 
+#install docker
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+#clone repository for hadoop lab sample
+git clone ${git_clone_url}
+cd ${project_name}
+echo '${DEFINITIONS}' > .definitions.sh
+sudo docker build -t bot .
+sudo docker run -it --name bot bot
