d1f471620d8821ef630412ae1960e25ef15fb789
Rodolfo Neto <rodolfotiago@gmail.com>
2019-12-12 14:05:27 -0300

    [custom-vm]|odroid-contas-bot.sh

--- scripts/odroid-contas-bot.sh
+++ scripts/odroid-contas-bot.sh
@@ -8,13 +8,14 @@ sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt-get update
 sudo apt-get install -y docker-ce
 
-#clone repository for hadoop lab sample
+#clone odroid-contas repository
 git clone ${git_clone_url}
 cd ${project_name}
+
+#inject definitions properties
 echo -e '
 export TELEGRAM_TOKEN=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/attributes/TELEGRAM_TOKEN)
 export NOTIFICATION_IDS=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/attributes/NOTIFICATION_IDS)
@@ -43,5 +44,6 @@ export GNU_PLOT_DAT="${BASEDIR}/configs/test.dat"
 export STATS_LOG_PATH="/home/odroid/telegram_bots_logs/contas_"
 ' > .definitions.sh
 
+#run bot in docker
 sudo docker build -t bot .
 sudo docker run -i --name bot bot
