00704738a881127c9d20f31fbe0e0f1dcedd3892
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 14:31:48 -0300

    Update pubsub-topics.tf

--- terraform/pubsub-topics.tf
+++ terraform/pubsub-topics.tf
@@ -1,7 +1,7 @@
 resource "google_pubsub_topic" "start-event-topic" {
-  name = "${var.start_topic}"
+  name = var.start_topic
 }
 
 resource "google_pubsub_topic" "stop-event-topic" {
-  name = "${var.stop_topic}"
+  name = var.stop_topic
 }
