9d05e1b9d1b16f3c56d836e9df4c063e05074c1f
Rodolfo Neto <rodolfotiago@gmail.com>
2020-02-28 14:31:33 -0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("${var.key}")}"
-  project     = "${var.project_id}"
-  region      = "${var.region}"
+  credentials = "${file(var.key)}"
+  project     = var.project_id
+  region      = var.region
 }
