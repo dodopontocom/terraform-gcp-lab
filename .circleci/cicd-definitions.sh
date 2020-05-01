dc42b79bb97365da8abaa5f41923114956eeac03
rodolfotiago <rodolfotiago@gmail.com>
2020-05-01 12:21:28 -0300

    json file

--- .circleci/cicd-definitions.sh
+++ .circleci/cicd-definitions.sh
@@ -4,8 +4,8 @@
 export GCLOUD_PROJECT_ID="construtora-2020"
 
 # GCLOUD service account credentials path
-export GOOGLE_APPLICATION_CREDENTIALS=${GCLOUD_JSON_KEY_PATH}
 export GCLOUD_JSON_KEY_PATH="${CIRCLE_WORKING_DIRECTORY}/credentials/credential.json"
+export GOOGLE_APPLICATION_CREDENTIALS=${GCLOUD_JSON_KEY_PATH}
 export GCLOUD_PROJECT_BUCKET_NAME="www.construtoracarvalhopereira.com.br"
 export GCLOUD_PROJECT_REGION="southamerica-east1"
 
